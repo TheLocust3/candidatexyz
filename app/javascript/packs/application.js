c83a2f8f758135650c2d516a14147d7cd97393cf
TheLocust3 <jake.kinsella@gmail.com>
2018-03-01 18:13:34 -0500

    Initial commit. Base of rails-boilerpplate

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,64 @@
+import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+
+import reducer from '../components/reducers/root-reducer';
+
+import Index from '../components/containers/Index';
+import SignInContainer from '../components/containers/users/SignInContainer';
+import SignUpContainer from '../components/containers/users/SignUpContainer';
+import ForgotPasswordContainer from '../components/containers/users/ForgotPasswordContainer';
+import ResetPasswordContainer from '../components/containers/users/ResetPasswordContainer';
+import EditUserContainer from '../components/containers/users/EditUserContainer';
+
+$.ajaxSetup({
+    headers: {
+        'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+    },
+    dataType: 'json'
+});
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware));
+
+// Always start navigation at the top of the page
+const ScrollToTop = () => {
+    window.scrollTo(0, 0);
+
+    return null;
+};
+
+class Base extends React.Component {
+
+    render() {
+        return (
+            <Provider store={store}>
+                <div>
+                    <Router>
+                        <div>
+                            <Route component={ScrollToTop} />
+
+                            <Switch>
+                                <Route exact path="/" component={Index} />
+                                <Route exact path="/sign_in" component={SignInContainer} />
+                                <Route exact path="/sign_up" component={SignUpContainer} />
+                                <Route exact path="/forgot_password" component={ForgotPasswordContainer} />
+                                <Route exact path="/reset_password" component={ResetPasswordContainer} />
+                                <Route exact path="/users/edit" component={EditUserContainer} />
+
+                            </Switch>
+                        </div>
+                    </Router>
+                </div>
+            </Provider>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Base />,
+    document.getElementById('root')
+);
---
+++ terraform.tf
@@ -0,0 +1,196 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "name" {
+  default = "test"
+}
+
+variable "username" {
+  default = "testusername"
+}
+
+data "aws_ami" "image" {
+  name_regex = "ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server"
+  most_recent = true
+}
+
+resource "random_id" "database_password" {
+  keepers = {
+    password = "${var.username}"
+  }
+
+  byte_length = 16
+}
+
+resource "random_id" "random" { // Using this for snapshot names
+  keepers = {
+    password = "${var.name}"
+  }
+
+  byte_length = 8
+}
+
+resource "aws_security_group" "security_group" {
+  name = "${var.name} security group"
+  description = "Managed by Terraform"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "db_security_group" {
+  name = "${var.name} rds security group"
+  description = "Managed by Terraform"
+
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    security_groups = ["${aws_security_group.security_group.id}"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+data "aws_iam_policy_document" "ec2-role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ec2-role" {
+  name = "${var.name}-ec2"
+  assume_role_policy = "${data.aws_iam_policy_document.ec2-role.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "ec2-role-for-codedeploy" {
+  role = "${aws_iam_role.ec2-role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforAWSCodeDeploy"
+}
+
+resource "aws_iam_instance_profile" "ec2-profile" {
+  name  = "${var.name}_profile"
+  role = "${aws_iam_role.ec2-role.name}"
+}
+
+
+data "aws_iam_policy_document" "codedeploy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "Service"
+      identifiers = ["codedeploy.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "deployment" {
+  name = "${var.name}-deployment"
+
+  assume_role_policy = "${data.aws_iam_policy_document.codedeploy.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "deployment" {
+  role = "${aws_iam_role.deployment.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
+}
+
+
+resource "aws_instance" "instance" {
+  ami = "${data.aws_ami.image.id}"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.security_group.id}"]
+  associate_public_ip_address = true
+  iam_instance_profile = "${aws_iam_instance_profile.ec2-profile.name}"
+  key_name = "${var.name}"
+
+  tags {
+    Name = "${var.name}"
+  }
+}
+
+resource "aws_db_instance" "db_instance" {
+  identifier = "${var.name}"
+  allocated_storage = "5"
+  storage_type = "gp2"
+  engine = "postgres"
+  engine_version = "9.6.3"
+  instance_class = "db.t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.db_security_group.id}"]
+
+  final_snapshot_identifier = "${var.name}-final-snapshot-${random_id.database_password.hex}"
+
+  name = "${var.name}"
+  username = "${var.username}"
+  password = "${random_id.database_password.hex}"
+}
+
+resource "aws_codedeploy_app" "application" {
+  name = "${var.name}"
+}
+
+resource "aws_codedeploy_deployment_group" "deployment" {
+  app_name = "${aws_codedeploy_app.application.name}"
+  deployment_group_name = "production"
+  service_role_arn = "${aws_iam_role.deployment.arn}"
+
+  ec2_tag_filter {
+    type = "KEY_AND_VALUE"
+    key = "Name"
+    value = "${var.name}"
+  }
+}
+
+output "instance_ip" {
+  value = "${aws_instance.instance.public_ip}"
+}
+
+output "database_username" {
+  value = "${aws_db_instance.db_instance.username}"
+}
+
+output "database_address" {
+  value = "${aws_db_instance.db_instance.address}"
+}
+
+output "database_password" {
+  value = "${aws_db_instance.db_instance.password}"
+}
