2b22827fd4eef5ffbec70bb5a5e33bd55700c0d9
TheLocust3 <jake.kinsella@gmail.com>
2018-03-04 16:28:05 -0500

    Use mdc theming and sass variables for colors

--- app/javascript/components/components/common/Navbar.jsx
+++ app/javascript/components/components/common/Navbar.jsx
@@ -10,8 +10,8 @@ export default class Navbar extends React.Component {
                     <img src="https://static1.squarespace.com/static/598dd363a5790a4026dac8ab/t/59933e3e7131a5f7bfb18c09/1518809293655/?format=1500w" className="navbar-image" />
 
                     <div className="navbar-actions">
-                        <div className="button-cell"><Link to="/meet"><button className="mdc-button button" data-mdc-auto-init="MDCRipple">Meet CandidateXYZ</button></Link></div>
-                        <div className="button-cell"><Link to="/action"><button className="mdc-button button" data-mdc-auto-init="MDCRipple">Take Action</button></Link></div>
+                        <div className="button-cell"><Link className='link' to="/meet">Meet CandidateXYZ</Link></div>
+                        <div className="button-cell"><Link className='link' to="/action">Take Action</Link></div>
                         <div className="button-cell"><Link to="https://www.actblue.com"><button className="mdc-button mdc-button--raised button" data-mdc-auto-init="MDCRipple">Donate</button></Link></div>
                     </div>
                 </div>
