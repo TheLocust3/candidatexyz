e30eebe7e6d5a11192dfe72c4b1758d279e4a3ef
TheLocust3 <jake.kinsella@gmail.com>
2018-03-16 21:29:41 -0400

    Mobile tweaks to join team panel

--- app/javascript/components/components/common/JoinTeamPanel.jsx
+++ app/javascript/components/components/common/JoinTeamPanel.jsx
@@ -27,16 +27,14 @@ export default class JoinTeamPanel extends React.Component {
         $('.join-team-panel-content').css('background-image', `url(${this.state.imageUrl})`);
 
         return (
-            <div className='join-team-panel'>
-                <div className='join-team-panel-content'>
-                    <div className='join-team-panel-title join-team-panel-color'>
-                        <div className='mdc-typography--display2'><b><TextContent identifier='joinTeamPanelTitle' /></b></div>
-                    </div>
+            <div className='join-team-panel-content'>
+                <div className='join-team-panel-title join-team-panel-color'>
+                    <div className='mdc-typography--display2'><b><TextContent identifier='joinTeamPanelTitle' /></b></div>
+                </div>
 
-                    <div className='join-team-panel-actions mdc-typography--headline'>
-                        <Link to="/sign_up"><button className="mdc-button mdc-button--raised button join-team-panel-color" data-mdc-auto-init="MDCRipple">Sign Up</button></Link>
-                        <ExternalLinkContent identifier='joinTeamPanelDonateLink'><button className="mdc-button mdc-button--raised button join-team-panel-color" data-mdc-auto-init="MDCRipple">Donate</button></ExternalLinkContent>
-                    </div>
+                <div className='join-team-panel-actions mdc-typography--headline'>
+                    <Link to="/sign_up"><button className="mdc-button mdc-button--raised button join-team-panel-color" data-mdc-auto-init="MDCRipple">Sign Up</button></Link>
+                    <ExternalLinkContent identifier='joinTeamPanelDonateLink'><button className="mdc-button mdc-button--raised button join-team-panel-color" data-mdc-auto-init="MDCRipple">Donate</button></ExternalLinkContent>
                 </div>
             </div>
         );
