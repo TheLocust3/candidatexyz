0294e5671aeeedffb815b62b5b93fb84c217a698
TheLocust3 <jake.kinsella@gmail.com>
2018-06-19 14:48:33 -0400

    Local flag that sets all api routes

--- app/javascript/constants.js
+++ app/javascript/constants.js
@@ -8,10 +8,11 @@ import { createBrowserHistory } from 'history';
 export let history = createBrowserHistory();
 export const store = createStore(reducer, applyMiddleware(thunkMiddleware));
 export const MAX_MOBILE_WIDTH = 768;
-export const DOMAIN = 'https://candidatexyz.com';
-// export const DOMAIN = 'http://127.0.0.1:3001';
 export const STATES = [ 'AL', 'AK', 'AZ', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY', 'AS', 'DC', 'FM', 'GU', 'MH', 'MP', 'PW', 'PR', 'VI' ];
-export const VOLUNTEER_API_DOMAIN = 'http://127.0.0.1:3002'; // Should be changed for production
-// export const VOLUNTEER_API_DOMAIN = 'https://api.candidatexyz.com';
-export const USER_API_DOMAIN = 'http://127.0.0.1:3003'; // Should be changed for production
-// export const USER_API_DOMAIN = 'https://auth.candidatexyz.com';
+
+export const LOCAL = false;
+
+export const DOMAIN = LOCAL ? 'http://127.0.0.1:3001' : 'https://candidatexyz.com';
+export const VOLUNTEER_API_DOMAIN = LOCAL ? 'http://127.0.0.1:3002' : 'https://api.candidatexyz.com';
+export const USER_API_DOMAIN = LOCAL ? 'http://127.0.0.1:3003' : 'https://auth.candidatexyz.com';
+export const MAILER_API_DOMAIN = LOCAL ? 'http://127.0.0.1:3004' : 'https://mailer.candidatexyz.com';
