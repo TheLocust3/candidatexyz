b60e68196ac00ecf2692a92525d6ecb296139ca1
TheLocust3 <jake.kinsella@gmail.com>
2018-06-21 14:55:38 -0400

    Setup common-js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,26 +2,15 @@ import $ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import * as Auth from 'j-toker';
 
-import { store, USER_API_DOMAIN, DOMAIN } from '../constants';
+import { store } from '../constants';
 
 import Routes from '../routes/Routes';
 
 $.ajaxSetup({
     headers: {
         'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
-    },
-    beforeSend: function(xhr, settings) {
-        $.auth.appendAuthHeaders(xhr, settings);
     }
-});
-
-$.auth.configure({
-    apiUrl: USER_API_DOMAIN,
-    passwordResetSuccessUrl: () => { return `${DOMAIN}/reset_password` },
-    storage: 'cookies',
-    cookieExpiry: 14,
 });
 
 class Base extends React.Component {
