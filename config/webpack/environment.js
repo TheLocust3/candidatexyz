bb8d9be985bd3a431efe079e4326c48d7e87e3bf
TheLocust3 <jake.kinsella@gmail.com>
2018-06-27 12:38:39 -0400

    Move crazy compile webpack logic to production only

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
-environment.loaders.prepend('babel', {
-    test: /\.js/,
-    exclude: /react-parallax/,
-    loader: 'babel-loader',
-    query: {
-        presets: ['react', 'es2015']
-    }
-});
-
 module.exports = environment
