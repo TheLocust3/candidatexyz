92253510525dbb7a19957d12da575766f2bd7fed
TheLocust3 <jake.kinsella@gmail.com>
2018-06-28 09:13:14 -0400

    Comment out super compile in development

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,12 +1,12 @@
 const environment = require('./environment')
 
-environment.loaders.prepend('babel', {
+/*environment.loaders.prepend('babel', {
     test: /\.js/,
     exclude: /react-parallax/,
     loader: 'babel-loader',
     query: {
         presets: ['react', 'env']
     }
-});
+});*/
 
 module.exports = environment.toWebpackConfig()
