e66106a2f8e21148b320f0e52f1ea43932f856f6
TheLocust3 <jake.kinsella@gmail.com>
2018-07-02 23:46:46 -0400

    Messing around with deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -10,13 +10,13 @@ DEPLOYMENT_GROUP=production
 git clone https://github.com/TheLocust3/candidatexyz-common.git common
 tar -czvf common.tar.gz common
 
+aws s3 mb s3://$BUCKET
 aws s3 rm s3://$BUCKET/common.tar.gz
 aws s3 cp common.tar.gz s3://$BUCKET/
 
 #git archive --format=tar.gz -9 -o deploy.tar.gz HEAD
 tar -czvf deploy.tar.gz *
 
-aws s3 mb s3://$BUCKET
 aws s3 rm s3://$BUCKET/deploy.tar.gz
 aws s3 cp deploy.tar.gz s3://$BUCKET/
 
